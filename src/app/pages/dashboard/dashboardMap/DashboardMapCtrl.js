9495bc37cae30b013f4a933bc7daa9b2a5d5d51f
Daniel Rodríguez <daniel.rodriguez182@gmail.com>
2017-10-26 00:27:12 -0500

    Ajuste mapa colombia y dashboard

--- src/app/pages/dashboard/dashboardMap/DashboardMapCtrl.js
+++ src/app/pages/dashboard/dashboardMap/DashboardMapCtrl.js
@@ -12,64 +12,26 @@
   function DashboardMapCtrl(baConfig, layoutPaths) {
     var layoutColors = baConfig.colors;
     var map = AmCharts.makeChart('amChartMap', {
-      type: 'map',
-      theme: 'blur',
-      zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
-
-      dataProvider: {
-        map: 'colombiaLow',
-        zoomLevel: 3.5,
-        getAreasFromMap: true,
-        zoomLevel: 0.9,
-        areas: []
+      "type": "map",
+      "theme": "light",
+      "colorSteps": 6,
+      "dataProvider": {
+        "map": "colombiaLow",
+        "getAreasFromMap": true,
+        "zoomLevel": 0.9,
+        "areas": [{id: "CO-CAS", value: "1000"}]
       },
-
-      areasSettings: {
-        rollOverOutlineColor: layoutColors.border,
-        rollOverColor: layoutColors.primaryDark,
-        alpha: 0.8,
-        unlistedAreasAlpha: 0.2,
-        unlistedAreasColor: layoutColors.defaultText,
-        balloonText: '[[title]]: [[customData]] users'
+      "areasSettings": {
+        "autoZoom": true,
+        "balloonText": "[[title]]: <strong>[[value]]</strong>"
       },
-
-
-      legend: {
-        width: '100%',
-        marginRight: 27,
-        marginLeft: 27,
-        equalWidths: false,
-        backgroundAlpha: 0.3,
-        backgroundColor: layoutColors.border,
-        borderColor: layoutColors.border,
-        borderAlpha: 1,
-        top: 362,
-        left: 0,
-        horizontalGap: 10,
-        data: [
-          {
-            title: 'over 1 000 users',
-            color: layoutColors.primary
-          },
-          {
-            title: '500 - 1 000 users',
-            color: layoutColors.successLight
-          },
-          {
-            title: '100 - 500 users',
-            color: layoutColors.success
-          },
-          {
-            title: '0 - 100 users',
-            color: layoutColors.danger
-          }
-        ]
+      "valueLegend": {
+        "minValue": "little",
+        "maxValue": "a lot!"
       },
-      export: {
-        enabled: true
-      },
-      creditsPosition: 'bottom-right',
-      pathToImages: layoutPaths.images.amChart
+      "zoomControl": {
+        "minZoomLevel": 0.9
+      }
     });
   }
 })();
